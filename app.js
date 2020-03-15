b2eb679f3534ba298d0f4340fef0311e565d60a4
Bodhisha Thomas <bodhisha@gmail.com>
2020-03-15 20:54:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const modelParams = {
-  flipHorizontal: true, // flip e.g for video
-  imageScaleFactor: 0.7, // reduce input image size for gains in speed.
-  maxNumBoxes: 1, // maximum number of boxes to detect
-  iouThreshold: 0.5, // ioU threshold for non-max suppression
-  scoreThreshold: 0.79 // confidence threshold for predictions.
+    flipHorizontal: true, // flip e.g for video
+    imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+    maxNumBoxes: 1, // maximum number of boxes to detect
+    iouThreshold: 0.5, // ioU threshold for non-max suppression
+    scoreThreshold: 0.79 // confidence threshold for predictions.
 };
 //getting webcam
 navigator.getUserMedia =
-  navigator.getUserMedia ||
-  navigator.webkitGetUserMedia ||
-  navigator.mozGetUserMedia ||
-  navigator.msGetUserMedia;
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
 
 //select everything from html
 
@@ -20,10 +20,42 @@ const audio = document.querySelector("#audio");
 let model;
 
 handTrack.startVideo(video).then(status => {
-  if (status) {
-    navigator.getUserMedia({ video: {} }, stream => {
-      video.srcObject = stream;
-      //Run our detection
-    });
-  }
+    if (status) {
+        navigator.getUserMedia({ video: {} }, stream => {
+                video.srcObject = stream;
+                //Run our detection
+                setInterval(runDetection, 100)
+
+            },
+            err => console.log(err)
+        );
+    }
 });
+
+function runDetection() {
+    model.detect(video).then(predictions => {
+        if (predictions.length !== 0) {
+            let hand1 = predictions[0].bbox;
+            let x = hand1[0];
+            let y = hand1[1]
+            if (y > 300) {
+                if (x < 200) {
+                    audio.src = "a.mp3"
+                } else if (x > 400) {
+                    audio.src = "e.mp3"
+                } else if (x > 300) {
+                    audio.src = "c.mp3"
+                } else if (x > 200) {
+                    audio.src = "b.mp3"
+                }
+            }
+            //play audio
+            audio.play()
+        }
+    })
+}
+
+handTrack.load(modelParams).then(lmodel => {
+    model = lmodel
+
+})
